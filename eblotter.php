ccbab53d53234c4f481afde15e7a3d3b9bd95f52
jaydeeeeeeeeeee <dedase.23402094m.bsap@gmail.com>
2026-05-22 02:12:23 +0800

    Delete eblotter.php

--- eblotter.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if(!isset($_SESSION['admin'])){header("Location: admin.php");exit();}
-include 'role_helper.php';
-if($is_guest){header("Location: Home.php?denied=eblotter");exit();}
-header("Location: ../eBlotter/eblotter_home.php");
-exit();
